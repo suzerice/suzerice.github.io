3036359091cd7c97ee6a0782e25e580f785a63f1
suzerice <suzerice@users.noreply.github.com>
2019-09-09 12:58:27 -0400

    Create email-submit.php

---
+++ scripts/email-submit.php
@@ -0,0 +1,13 @@
+if($_POST["submit"]) {
+    $recipient="contact@folktalebakery.com";
+    $subject="Folktale Message!";
+    $sender=$_POST["sender"];
+    $senderEmail=$_POST["senderEmail"];
+    $message=$_POST["message"];
+
+    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+
+    $thankYou="<p>Thank you! Your message has been sent.</p>";
+}
